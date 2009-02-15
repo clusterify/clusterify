cdabebc93c11431c2a1d3897671fad9010bb6eee
fsav <francois@fsavard.com>
2009-02-15 17:29:07 +0000

    Added a script to set up env vars for prod environment (fsavard).

---
+++ settings_files/prod_env.sh
@@ -0,0 +1,3 @@
+export DJANGO_SETTINGS_MODULE=clusterify.settings_files.prod
+export PYTHONPATH=$PYTHONPATH:/home/fsavard/webapps/django/lib/python2.5:/home/fsavard/webapps/django/:/home/fsavard/webapps/django/clusterify
+export DJADMIN=/home/fsavard/webapps/django/bin/django-admin.py
