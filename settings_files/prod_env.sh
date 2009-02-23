ae4d19bea5b49b275e50e61ef41198c57ead6be6
fsav <francois@fsavard.com>
2009-02-23 02:38:06 +0000

    Added ability to set a project as needing an admin.

--- settings_files/prod_env.sh
+++ settings_files/prod_env.sh
@@ -1,3 +1,3 @@
 export DJANGO_SETTINGS_MODULE=clusterify.settings_files.prod
-export PYTHONPATH=$PYTHONPATH:/home/fsavard/webapps/django/lib/python2.5:/home/fsavard/webapps/django/:/home/fsavard/webapps/django/clusterify
+export PYTHONPATH=/home/fsavard/webapps/django/lib/python2.5:`pwd`/../..:`pwd`/..
 export DJADMIN=/home/fsavard/webapps/django/bin/django-admin.py
