f6dca90dede29568a9f7eba9b1b642e00351613b
webiest <webiest@gmail.com>
2009-03-27 05:19:12 +0000

    adds new filters for stats counts.

--- projects/templatetags/projects_filters.py
+++ projects/templatetags/projects_filters.py
@@ -1,6 +1,7 @@
 from django import template
+from projects.models import Project
+from django.contrib.auth.models import User
 
-from projects.models import Seed
 
 register = template.Library()
 
@@ -24,14 +25,36 @@ def do_voted_on_seed(user, seed):
         raise template.TemplateSyntaxError("user_voted_on_seed tag takes exactly 6 arguments")
     if bits[2] != 'on':
         raise template.TemplateSyntaxError("second argument to 'user_voted_on_seed' tag must be 'on'")
-	if bits[4] != 'as':
+    if bits[4] != 'as':
         raise template.TemplateSyntaxError("fourth argument to 'user_voted_on_seed' tag must be 'as'")
-	
-	user = template.resolve_variable(bits[1])
-	seed = template.resolve_variable(bits[3])
-	
+    
+    user = template.resolve_variable(bits[1])
+    seed = template.resolve_variable(bits[3])
+    
     return SetContextVarNode(bits[5], seed.user_voted(user))
 
 
 register.tag('user_voted_on_seed', do_voted_on_seed)
 
+
+def proposed_project_count():
+    return Project.objects.filter(p_completed=False, wont_be_completed=False).count()
+
+register.simple_tag(proposed_project_count)
+
+def completed_project_count():
+    return Project.objects.filter(p_completed=True, wont_be_completed=False).count()
+
+register.simple_tag(completed_project_count)
+
+def people_count():
+    return User.objects.all().count()
+
+register.simple_tag(people_count)
+
+@register.simple_tag
+def active(request, pattern):
+    import re
+    if re.search(pattern, request.path):
+        return 'active'
+    return ''
