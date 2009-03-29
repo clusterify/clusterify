583a9675601666d21be742eccba694ac178f6e5b
fsav <francois@fsavard.com>
2009-03-29 09:45:49 +0000

    More cleanup

--- utils.py
+++ utils.py
@@ -11,6 +11,11 @@ from django.contrib.sites.models import Site
 def get_full_url(path_relative_to_site_root=""):
 	return 'http://%s%s' % (Site.objects.get_current().domain, path_relative_to_site_root)
 
+def oops(error_message=""):
+	return render_to_response('oops.html',
+		{'error_message': error_message},
+		context_instance=RequestContext(request))
+
 def generic_confirmation_view(request, question, url_yes, url_no):
 	return render_to_response("confirm.html",
 			{'question':question, 'url_yes':url_yes, 'url_no': url_no},
